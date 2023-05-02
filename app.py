eae9e7665b5836b1b63cb827a9367fb044b65e83
yizhouzhao <yizhou_zhao@berkeley.edu>
2023-05-02 16:08:46 -0700

    add ui test

--- app.py
+++ app.py
@@ -1,25 +1,5 @@
-from PIL import ImageGrab
 from pynput import keyboard
-
 from src.game_learn import GameLearn
-
-# Define a function to capture a screenshot
-def capture_screenshot():
-    # Capture the screen and save it to a file
-    im = ImageGrab.grab()
-    print("Captured screenshot")
-    im.show()
-    im.save('screenshot.png')
-
-# Define a function to handle key press events
-def on_press(key):   
-    # Check if the key combination is pressed
-    if key == keyboard.Key.print_screen:
-        # Capture a screenshot
-        capture_screenshot()
-    elif key == keyboard.Key.pause:
-        exit()
-
 
 if __name__ == "__main__":
     # game learn
