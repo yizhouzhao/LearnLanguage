6683edf925ae22f9b16f1f54d77eca19c738eeea
yizhouzhao <yizhou_zhao@berkeley.edu>
2023-05-01 20:22:16 -0700

    inot

---
+++ utils.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+def push_image_to_front(image_path: str):
+    """
+    Push an image to the front of the screen.
+    """
+    # Create the main window
+    root = tk.Tk()
+
+    # Load the image and create a PhotoImage object
+    image = tk.PhotoImage(file="text.png")
+
+    # Create a label with the image and add it to the window
+    label = tk.Label(root, image=image)
+    label.pack()
+
+    # Raise the label to the top of the window stack
+    label.lift()
+
+    # Start the main event loop
+    root.mainloop()
